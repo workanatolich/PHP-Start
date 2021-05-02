4660e606739c13e6f94407bfab8b61a1508debf0
anatolich <anatolich.work.space@gmail.com>
2021-05-03 01:43:04 +0300

    Lesson 1. File system. Functions 'is_file()' and 'is_dir()'

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 $files = array_diff(scandir('dir'), ['.','..']);
 
-foreach ($files as $elem) {
-    echo file_get_contents('dir/'.$elem) .'<br>';
+foreach ($files as $file) {
+    if(is_file('dir/'.$file)) {
+        echo 'File ' .$file .'<br>';
+    }
+
+    if(is_dir('dir/'.$file)) {
+        echo 'Directory ' .$file .'<br>';
+    }
+
 }
