adbc8289c655f90bb2adb11a4f160035fa0d5826
anatolich <anatolich.work.space@gmail.com>
2021-05-02 03:10:56 +0300

    Lesson 1. File system. Function 'file()'

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
 
-function getSum($filename) {
-    $data = file_get_contents($filename);
-    $array = explode(PHP_EOL, $data);
-    return array_sum($array);
-}
-
-
-file_put_contents('sum.txt', getSum('test1.txt'));
-
-$arr = [1,2,3,4,5,6,7,8,9,10];
-file_put_contents('test2.txt', implode(PHP_EOL, $arr));
-
+$sum = array_sum(file('test1.txt', FILE_IGNORE_NEW_LINES));
+file_put_contents('test1.txt', file_get_contents('test1.txt') .PHP_EOL .$sum);
