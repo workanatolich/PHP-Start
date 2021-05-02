79fc17135523d613e9656933f0509bf9938f6a4c
anatolich <anatolich.work.space@gmail.com>
2021-05-02 03:00:21 +0300

    Lesson 1. File system. Practice 'PHP_EOL'

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-$arr = ['1.txt', '2.txt', '3.txt'];
-foreach ($arr as $item) {
-    var_dump(filesize($item));
-    echo '<br>';
+function getSum($filename) {
+    $data = file_get_contents($filename);
+    $array = explode(PHP_EOL, $data);
+    return array_sum($array);
 }
+
+
+file_put_contents('sum.txt', getSum('test1.txt'));
+
+$arr = [1,2,3,4,5,6,7,8,9,10];
+file_put_contents('test2.txt', implode(PHP_EOL, $arr));
+
