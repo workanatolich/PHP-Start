981e8da26432defd0e9d1574d1a05e0b1aa8873e
anatolich <anatolich.work.space@gmail.com>
2021-05-10 02:38:29 +0300

    Lesson 4. SQL. Mysqli connect

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-phpinfo();
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+$host = 'database';
+$user = 'root';
+$password = 'test';
+$db_name = 'test';
+
+$link = mysqli_connect($host, $user, $password, $db_name);
+mysqli_query($link, "SET NAMES 'utf8'");
+
+$sql = "SELECT * FROM workers";
+$result = mysqli_query($link, $sql) or die(mysqli_error($link));
+
+for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+
+echo '<pre>';
+print_r($data);
+echo '</pre>';
