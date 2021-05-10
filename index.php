959d7dfb9bf25af2948eb12f8a68fde8ddcf20ba
anatolich <anatolich.work.space@gmail.com>
2021-05-11 02:18:20 +0300

    Lesson 4. SQL. Practice 'LIMIT'

--- index.php
+++ index.php
@@ -11,12 +11,8 @@ $db_name = 'test';
 $link = mysqli_connect($host, $user, $password, $db_name);
 mysqli_query($link, "SET NAMES 'utf8'");
 
-$sql1 = "UPDATE workers SET name='Женя', salary='900' WHERE name='Вася'";
-mysqli_query($link, $sql1) or die(mysqli_error($link));
-
-
-$sql2 = "SELECT * FROM workers";
-$result = mysqli_query($link, $sql2) or die(mysqli_error($link));
+$sql1 = "SELECT * FROM workers LIMIT 2,3";
+$result = mysqli_query($link, $sql1) or die(mysqli_error($link));
 
 for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
 
