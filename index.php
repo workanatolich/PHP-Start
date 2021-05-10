a9f9223b732e28d25269b00d8583e63ab5f44c01
anatolich <anatolich.work.space@gmail.com>
2021-05-10 03:14:49 +0300

    Lesson 4. SQL. Practice 'DELETE'

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ $db_name = 'test';
 $link = mysqli_connect($host, $user, $password, $db_name);
 mysqli_query($link, "SET NAMES 'utf8'");
 
-$sql1 = "INSERT INTO workers (name,age,salary) VALUES ('Ярослав', 30, 1200), ('Петя', 24, 1000)";
+$sql1 = "DELETE FROM workers WHERE id<20";
 mysqli_query($link, $sql1) or die(mysqli_error($link));
 
 
-$sql2 = "SELECT * FROM workers WHERE id>8";
+$sql2 = "SELECT * FROM workers";
 $result = mysqli_query($link, $sql2) or die(mysqli_error($link));
 
 for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
