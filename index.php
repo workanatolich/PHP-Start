ac5d0f2baaead9b4376a3258e36721da920dfdb4
anatolich <anatolich.work.space@gmail.com>
2021-05-12 00:51:15 +0300

    Lesson 4. SQL. Data output

--- index.php
+++ index.php
@@ -1,21 +1,47 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>name</th>
+        <th>age</th>
+        <th>salary</th>
+    </tr>
+    <?php
+        $link = mysqli_connect('database', 'root', 'test', 'test')
+                or die(mysqli_error($link));
+        mysqli_query($link, "SET NAMES 'utf8'");
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
+        $sql = "SELECT * FROM workers ";
+        $result = mysqli_query($link, $sql) or die(mysqli_error($link));
 
-$host = 'database';
-$user = 'root';
-$password = 'test';
-$db_name = 'test';
+        for($data=[]; $row=mysqli_fetch_assoc($result); $data[] = $row);
 
-$link = mysqli_connect($host, $user, $password, $db_name);
-mysqli_query($link, "SET NAMES 'utf8'");
+        $result = '';
+        foreach($data as $elem) {
+            $result .= '<tr>';
 
-$sql1 = "SELECT * FROM pages WHERE article LIKE '%элемент%'";
-$result = mysqli_query($link, $sql1) or die(mysqli_error($link));
+            $result .= '<td>' .$elem['id'] . '</td>';
+            $result .= '<td>' .$elem['name'] . '</td>';
+            $result .= '<td>' .$elem['age'] . '</td>';
+            $result .= '<td>' .$elem['salary'] . '</td>';
 
-for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+            $result .= '</tr>';
+        }
 
-echo '<pre>';
-print_r($data);
-echo '</pre>';
+        echo $result;
+    ?>
+</table>
+
+
+
+</body>
+</html>
