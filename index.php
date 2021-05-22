e3d6f21d7cafab61141af8ea6911c6597f2e5b84
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-22 18:44:20 +0300

    Lesson 6. Simple engine. Practice 'include' with ''

--- index.php
+++ index.php
@@ -3,8 +3,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-$arr = ['1.php', '2.php', '3.php'];
-
-foreach ($arr as $file) {
-    include $file;
-}
+$page = $_GET['page'];
+include "dir/$page.php";
