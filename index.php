944ec312f0127bf55f9c730f14ddb0ed5216e9c3
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-22 18:47:54 +0300

    Lesson 6. Simple engine. Practice 'include' with 'file_exists'

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 $page = $_GET['page'];
-include "dir/$page.php";
+$path = "dir/$page.php";
+
+if(file_exists($path)) {
+    include $path;
+} else {
+    echo 'Такого файла не существует';
+}
